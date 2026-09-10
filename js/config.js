b94a45ac5f2dc26f105a7f9963e7532a65591749
baifu6276 <baifu6276@gmail.com>
2026-09-10 20:36:07 +0800

    refactor: stage shared CSS and config files

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Refactor scaffold: shared configuration.
+// 此檔目前不接入正式 index.html；待模組拆分完成後再由測試分支引用。
+const WATER_CLOCK_CONFIG = Object.freeze({
+  LIFF_ID: "2011467618-lNiVqxsV",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzrmID3lIBP2bxM_qC4nLiEOBEcROB3sFRH9eZsHt--A6o4VZpvuf63pzW0ku4j-TTd0w/exec"
+});
